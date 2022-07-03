e378e74d8db6495db3c7e2b11ee6cbcc9cfbe4cf
abhishek18124 <abhishek18124@iiitd.ac.in>
2022-07-03 13:17:39 +0530

    Lecture 6

---
+++ July/Lecture 17 - 3rdJuly2022/001Permutations.cpp
@@ -0,0 +1,22 @@
+/*
+
+Given a string (sequence of characters) of length n, generate all of its permutations. 
+
+Example
+	
+	Input : inp[] = "abc"
+	Output: ["abc", "acb", "bac", "bca", "cba", "cab"]
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
+
+int main() {
+
+	char inp[] = "abc";
+
+	return 0;
+}
---
+++ July/Lecture 17 - 3rdJuly2022/003Ladder.cpp
@@ -0,0 +1,29 @@
+/*
+
+Suppose there is a ladder with n steps. A person standing at the bottom of the ladder and wants to 
+reach to its top. Assume the person can take atmost k steps at a time. Then, count the number 
+of ways, the person can reach the top of the ladder.
+
+Example 
+	Input : n = 4, k = 3
+	Output: 7
+	Explanation : [1, 1, 1, 1], 
+	              [1, 1, 2], 
+	              [1, 2, 1], 
+	              [2, 1, 1],
+	              [2, 2],
+	              [1, 3],
+	              [3, 1]
+
+*/
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int n = 4;
+	int k = 3;
+
+	return 0;
+}
